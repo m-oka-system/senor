eb546bf4a407a09a12b080a56b7e9bdb34751df4
MakotoOkada <shirokuma@MakotoOkadanoMacBook-puro.local>
2017-11-24 16:22:25 +0900

    Add name field to sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  # deviseでユーザーの名前の登録を許可する
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -6,4 +9,10 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     dashbords_home_path
   end
+
+  # deviseでユーザーの名前の登録を許可する
+  def configure_permitted_parameters
+  devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
 end
