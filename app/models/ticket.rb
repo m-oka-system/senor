c0f15aed0e1eb3803876bd2002b0d0cf33594f7b
MakotoOkada <shirokuma@MakotoOkadanoMacBook-puro.local>
2018-01-01 15:41:14 +0900

    Make Ticket model

---
+++ app/models/ticket.rb
@@ -0,0 +1,11 @@
+class Ticket < ActiveRecord::Base
+  belongs_to :store
+
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :status, presence: true
+  validates :receive_date, presence: true
+  validates :receive_time, presence: true
+  validates :content, length: { maximum: 50 }
+  validates :result, length: { maximum: 50 }
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171217045846) do
+ActiveRecord::Schema.define(version: 20180101041859) do
 
   create_table "customers", force: true do |t|
     t.string   "customer_name"
@@ -42,6 +42,28 @@ ActiveRecord::Schema.define(version: 20171217045846) do
     t.string   "business_hours_end"
     t.string   "pos_type"
     t.string   "ip_address"
+    t.text     "remarks"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tickets", force: true do |t|
+    t.string   "store_code",       null: false
+    t.text     "title",            null: false
+    t.string   "status",           null: false
+    t.string   "sender"
+    t.string   "received_user_id"
+    t.string   "assign_user_id"
+    t.date     "receive_date",     null: false
+    t.time     "receive_time",     null: false
+    t.date     "close_date"
+    t.time     "close_time"
+    t.date     "esc_date"
+    t.time     "esc_time"
+    t.date     "limit_date"
+    t.text     "content"
+    t.text     "detail"
+    t.text     "result"
     t.text     "remarks"
     t.datetime "created_at"
     t.datetime "updated_at"
